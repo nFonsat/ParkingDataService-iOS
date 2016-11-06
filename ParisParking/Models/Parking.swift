31a0a71736f6591b9b92973ce46d6e0dd3976a32
Fonsat Nicolas <fonsat.nicolas@gmail.com>
2016-11-06 13:27:20 +0100

    add parking models

---
+++ ParisParking/Models/Parking.swift
@@ -0,0 +1,21 @@
+//
+//  Parking.swift
+//  ParisParking
+//
+//  Created by Nicolas Fonsat on 06/11/2016.
+//  Copyright © 2016 Nicolas Fonsat. All rights reserved.
+//
+
+import Foundation
+import CoreLocation
+
+class Parking {
+    
+    var idApi: Int!;
+    var nbPlace: Int!;
+    var type: String!;
+    var regime: String!;
+    var stationnement: String!;
+    var coordinate: CLLocationCoordinate2D!;
+
+}
