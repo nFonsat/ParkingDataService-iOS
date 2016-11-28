cbcaeee9570cf37f1ecba2df8629bab960c80136
Nicolas Fonsat <fonsat.nicolas@gmail.com>
2016-11-28 15:25:09 +0100

    Add TableViewCell

--- ParisParking/Views/MapViewController.swift
+++ ParisParking/Views/MapViewController.swift
@@ -12,8 +12,11 @@ import CoreLocation
 import SwiftyJSON
 
 class MapViewController: UIViewController  {
-
+    
     @IBOutlet weak var mapView: MKMapView!
+    @IBOutlet weak var placeTableView: UITableView!
+    
+    let cellId: String = "PlaceCell";
     
     let clusteringManager = FBClusteringManager();
     let configuration     = FBAnnotationClusterViewConfiguration.default();
@@ -24,17 +27,20 @@ class MapViewController: UIViewController  {
     
     var hasFinishRendering = false;
     
+    var places:[Parking] = [];
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        initTableView()
         mapViewInit()
         
         clusteringManager.delegate = self;
         
         /*
-        let template = "http://tile.openstreetmap.org/{z}/{x}/{y}.png"
-        self.mapViewTemplate(template)
-        */
+         let template = "http://tile.openstreetmap.org/{z}/{x}/{y}.png"
+         self.mapViewTemplate(template)
+         */
         
         self.title = "Paris..."
         
@@ -45,6 +51,16 @@ class MapViewController: UIViewController  {
     
     override func viewWillAppear(_ animated: Bool) {
         hasFinishRendering = false;
+    }
+    
+    func alertForParking(_ parking: Parking) {
+        let alertCtrl:UIAlertController = UIAlertController(title: "Choose", message: "Choose your action ?", preferredStyle: .actionSheet);
+        alertCtrl.addAction(UIAlertAction(title: "GO", style: .default, handler: { (action) in
+            self.mapView.showAnnotations([parking, self.mapView.userLocation], animated: true);
+            self.goToPlace(parking);
+        }));
+        alertCtrl.addAction(UIAlertAction(title: "Cancel", style: .destructive, handler: nil));
+        self.present(alertCtrl, animated: true, completion: nil);
     }
     
     func goToSearchView() {
@@ -84,14 +100,21 @@ extension MapViewController : FBClusteringManagerDelegate {
 
 extension MapViewController : MKMapViewDelegate {
     
-    func getParkings(bottom: CLLocationCoordinate2D, top: CLLocationCoordinate2D) {
+    func getParkings(bottom: CLLocationCoordinate2D, top: CLLocationCoordinate2D,  center: CLLocationCoordinate2D) {
         var parkings:[Parking] = []
         
-        self.geoService.parkings(min: bottom, max: top).responseJSON { response in
+        self.geoService.parkings(min: bottom, max: top, center: center).responseJSON { response in
+            var count = 0;
+            self.places.removeAll();
             if let result = response.result.value {
                 let jsonResult = JSON(result);
                 for value in jsonResult["result"].array! {
-                    parkings.append(ParkingFactory.getParkingFromJson(value.object as! [String : AnyObject]));
+                    let parking:Parking = ParkingFactory.getParkingFromJson(value.object as! [String : AnyObject]);
+                    parkings.append(parking);
+                    if count < 10 {
+                        self.places.append(parking);
+                        count+=1;
+                    }
                 }
             }
             print("-- getParkings() : \(top.latitude);\(top.longitude) <=> \(bottom.latitude);\(bottom.longitude) -- \(parkings.count)");
@@ -99,6 +122,7 @@ extension MapViewController : MKMapViewDelegate {
             DispatchQueue.main.async(execute: {
                 self.clusteringManager.removeAll();
                 self.clusteringManager.add(annotations: parkings);
+                self.placeTableView.reloadData();
                 DispatchQueue.global(qos: .userInitiated).async {
                     let mapBoundsWidth = Double(self.mapView.bounds.size.width)
                     let mapRectWidth = self.mapView.visibleMapRect.size.width
@@ -129,23 +153,68 @@ extension MapViewController : MKMapViewDelegate {
         mapView.add(overlay, level: .aboveLabels)
     }
     
+    func zoomOnMap(_ location: CLLocationCoordinate2D, meters: Double) {
+        let region:MKCoordinateRegion = MKCoordinateRegionMakeWithDistance(location, meters, meters);
+        self.mapView.setRegion(region, animated: true);
+    }
+    
+    func goToPlace(_ place:Parking) {
+        let request:MKDirectionsRequest = MKDirectionsRequest();
+        request.source = MKMapItem.forCurrentLocation();
+        
+        if #available(iOS 10.0, *) {
+            let destination:MKPlacemark = MKPlacemark(coordinate: place.coordinate);
+            request.destination = MKMapItem(placemark: destination);
+        } else {
+            let destination:MKPlacemark = MKPlacemark(coordinate: place.coordinate, addressDictionary: nil);
+            request.destination = MKMapItem(placemark: destination);
+        };
+        
+        
+        request.transportType = .automobile;
+        
+        let directions:MKDirections = MKDirections(request: request);
+        directions.calculate { (response, error) in
+            if let msg = error {
+                print("Error - goToPlace", msg.localizedDescription);
+            }
+            else if response != nil {
+                let details:MKRoute = (response?.routes.last)!;
+                
+                self.mapView.removeOverlays(self.mapView.overlays);
+                self.mapView.add(details.polyline);
+                
+                for step:MKRouteStep in details.steps {
+                    print("\(step.instructions) -- \(step.distance)");
+                }
+            }
+        }
+    }
+    
     func mapViewDidFinishRenderingMap(_ mapView: MKMapView, fullyRendered: Bool) {
         hasFinishRendering = fullyRendered;
     }
-
+    
     func mapView(_ mapView: MKMapView, regionDidChangeAnimated animated: Bool) {
         
         if hasFinishRendering {
             let bounds = self.mapView.getBoundingBox();
             let top = bounds[0];
             let bottom = bounds[1];
-            self.getParkings(bottom: bottom, top: top);
+            let userLoc = self.mapView.userLocation;
+            self.getParkings(bottom: bottom, top: top, center: userLoc.coordinate);
         }
     }
     
     func mapView(_ mapView: MKMapView, rendererFor overlay: MKOverlay) -> MKOverlayRenderer {
         if overlay.isKind(of: MKTileOverlay.self) {
             return MKTileOverlayRenderer(overlay: overlay)
+        }
+        else if overlay.isKind(of: MKPolyline.self) {
+            let renderer:MKPolylineRenderer = MKPolylineRenderer(polyline: overlay as! MKPolyline);
+            renderer.strokeColor = UIColor.brown;
+            renderer.lineWidth = 3;
+            return renderer;
         }
         
         return MKOverlayRenderer(overlay: overlay)
@@ -170,5 +239,40 @@ extension MapViewController : MKMapViewDelegate {
         
         return nil;
     }
+}
+
+extension MapViewController: UITableViewDelegate, UITableViewDataSource {
+    
+    func initTableView() {
+        let nib:UINib = UINib(nibName: "PlaceTableViewCell", bundle: nil);
+        self.placeTableView.register(nib, forCellReuseIdentifier: cellId);
+        
+        self.placeTableView.delegate = self;
+        self.placeTableView.dataSource = self;
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.places.count;
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell:PlaceTableViewCell = tableView.dequeueReusableCell(withIdentifier: cellId) as! PlaceTableViewCell;
+        
+        let place = self.places[indexPath.row];
+        cell.setPlace(place);
+        
+        return cell;
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let place = self.places[indexPath.row];
+        self.zoomOnMap(place.coordinate, meters: 3);
+        self.alertForParking(place);
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 50;
+    }
+    
 }
 
