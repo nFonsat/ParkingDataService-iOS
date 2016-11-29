13d6390dfe58e2d2acdefb98fd437c01abd76540
Nicolas Fonsat <fonsat.nicolas@gmail.com>
2016-11-29 16:20:05 +0100

    Change ParkingFactory

--- ParisParking/Factory/ParkingFactory.swift
+++ ParisParking/Factory/ParkingFactory.swift
@@ -14,16 +14,24 @@ class ParkingFactory {
     
     class func getParkingFromJson(_ data: [String:AnyObject]) -> Parking {
         let id:String = data["id_api"] as! String;
-        
+        let address:String = data["address_geo"] as! String;
+        let isParc:Bool = data["is_parc"] as! Bool;
+        let typeParking:String = data["type_parking"] as! String;
+        let typeStationnement:String = data["type_stationnement"] as! String;
+        let posStationnement:String = data["position_stationnement"] as! String;
         let lat = Double(data["lat"] as! String);
         let lng = Double(data["lng"] as! String);
+        let distance = Double((data["distance"] as? String)!);
         
-        let parking:Parking = Parking(id: id, coord: CLLocationCoordinate2DMake(lat!, lng!))
+        let parking:Parking = Parking(id, address: address, coord: CLLocationCoordinate2DMake(lat!, lng!));
         
-        //parking.nbPlace = data["total_place"] as! Int;
-        parking.type = data["type_parking"] as! String;
-        parking.regime = data["regime"] as! String;
-        parking.stationnement = data["type_stationnement"] as! String;
+        parking.name = data["name_parking"] as? String;
+        parking.isParc = isParc;
+        parking.typeParking = typeParking;
+        parking.posStationnement = posStationnement;
+        parking.typeStationnement = typeStationnement;
+        
+        parking.meters = lround(distance!);
         
         return parking;
     }
