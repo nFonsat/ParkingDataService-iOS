0c855b67a97bea683efd162ac8e35343270854ce
Nicolas Fonsat <fonsat.nicolas@gmail.com>
2016-11-29 13:29:24 +0100

    add swift test and fast lane

---
+++ ParisParkingTests/ParisParkingTests.swift
@@ -0,0 +1,24 @@
+//
+//  ParisParkingTests.swift
+//  ParisParkingTests
+//
+//  Created by Nicolas Fonsat on 29/11/2016.
+//  Copyright © 2016 Nicolas Fonsat. All rights reserved.
+//
+
+import XCTest
+@testable import ParisParking;
+
+class ParisParkingTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+}
