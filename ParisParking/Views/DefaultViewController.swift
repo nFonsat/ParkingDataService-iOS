66eb240697403137a1424c2e03ec8fff789cf6e3
Nicolas Fonsat <fonsat.nicolas@gmail.com>
2016-11-29 20:32:02 +0100

    Add defaultView

---
+++ ParisParking/Views/DefaultViewController.swift
@@ -0,0 +1,35 @@
+//
+//  DefaultViewController.swift
+//  ParisParking
+//
+//  Created by Nicolas Fonsat on 29/11/2016.
+//  Copyright © 2016 Nicolas Fonsat. All rights reserved.
+//
+
+import UIKit
+
+class DefaultViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
