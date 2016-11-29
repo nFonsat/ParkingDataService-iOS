ff1f2375e41f5122a77f5d4bbcfc3b15b87a8d69
Nicolas Fonsat <fonsat.nicolas@gmail.com>
2016-11-29 11:46:59 +0100

    Add custom Cell

---
+++ ParisParking/Components/TableViewCell/PlaceTableViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  PlaceTableViewCell.swift
+//  ParisParking
+//
+//  Created by Nicolas Fonsat on 29/11/2016.
+//  Copyright © 2016 Nicolas Fonsat. All rights reserved.
+//
+
+import UIKit
+
+class PlaceTableViewCell: UITableViewCell {
+    
+    func setPlace(_ place: Parking) {
+        self.textLabel?.text = "TEST";
+    }
+    
+}
